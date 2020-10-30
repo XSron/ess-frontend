bcd40a612522337d220e8b745d4668ccb8b198ad
XSronHou <houxsron@gmail.com>
2020-10-30 11:07:41 -0500

    Refactor AppConfig & Endpoint

--- src/app/services/credit-card.service.ts
+++ src/app/services/credit-card.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-<<<<<<< HEAD
-import { CreditCardModel, CreditCardType } from '../model/CreditCardModel';
 import { AppConfig } from '../common/app-config';
 import { Endpoint } from '../common/endpoint';
-=======
 import { CreditCardModel } from '../model/CreditCardModel';
-import { Endpoint } from '../common/global-constants';
->>>>>>> eed72dc29939e114a3c5f3702fda00c3679fb845
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +13,13 @@ export class CreditCardService {
   constructor(private httpClient: HttpClient) { }
 
   // MARK: - Credit Card Services
-
   getCreditCard(): any {
     return this.httpClient.get<CreditCardModel[]>(Endpoint.PAYMENT_ENDPOINT.CARD_VERIFICATION_ENDPOINT);
   }
-
   addCreditCard(body): any {
     return this.httpClient.post(Endpoint.PAYMENT_ENDPOINT.CARD_VERIFICATION_ENDPOINT, body);
   }
-
   deleteCreditCard(id): any {
     return this.httpClient.delete(Endpoint.PAYMENT_ENDPOINT.CARD_VERIFICATION_ENDPOINT + `\${id}`);
   }
-
 }
