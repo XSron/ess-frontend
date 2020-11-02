32f504a7140266d39a0f28bd93c0bed8a1c3ee49
Huy D <minhhuy.uit@gmail.com>
2020-11-02 15:36:26 -0600

    Update page not found layout.

--- src/app/components/fixed-blocks/notfound/notfound.component.ts
+++ src/app/components/fixed-blocks/notfound/notfound.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { cpuUsage } from 'process';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
-    selector: 'not-found',
-    templateUrl: 'notfound.component.html',
+  selector: 'not-found',
+  templateUrl: 'notfound.component.html',
+  styleUrls: ['./notfound.component.css']
 })
-export class NotFoundComponent {
-    
+
+export class NotFoundComponent implements OnInit, OnDestroy {
+
+  // MARK: - Properties
+
+
+  // MARK: - Angular Core functions
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  // MARK: - NotFoundComponent functions
+
 }
